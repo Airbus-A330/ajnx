21cf9011ee977425de4a738867fefd999ec05f31
Airbus-A330 <united.vietnam.airlines@gmail.com>
2025-04-24 17:58:19 -0400

    remove debugging from auth middleware

--- functions/requireAuth.js
+++ functions/requireAuth.js
@@ -6,14 +6,11 @@ function requireAuth(req, res, next) {
         return res.status(401).json({ error: "Missing Authorization header" });
 
     const token = authHeader.split(" ")[1];
-    console.log("Token:", token);
     try {
         const user = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("Decoded user:", user);
         req.user = user;
         next();
     } catch (err) {
-        console.log("Token verification error:", err.stack);
         res.status(403).json({ error: "Invalid token" });
     }
 }
