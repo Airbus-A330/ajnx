c39f7eb87d14ffbb6d3f869b915c10c989e6927a
Airbus-A330 <united.vietnam.airlines@gmail.com>
2025-04-24 01:13:24 -0400

    anonymize password

--- functions/db.js
+++ functions/db.js
@@ -3,8 +3,8 @@ const pc = require("picocolors");
 
 const pool = mysql.createPool({
   host: 'localhost',     // e.g., 'localhost' or '127.0.0.1'
-  user: 'root',
-  password: 'dbR!A@9_IpB0iVOH',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: 'AJNXBanking',
   waitForConnections: true,
   connectionLimit: 10,
